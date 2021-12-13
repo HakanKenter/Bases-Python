be6ec8a1aec6809f0326ce792fccb3d1bb99fcb4
Hakan Kenter <hakan.kt@outlook.fr>
2021-12-13 10:45:44 +0100

    First-commit

---
+++ FirstScript.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+'''
+Les triple guillements pour les commentaires sur plusieurs lignes
+En python, le caractère # sert à commencer un commentaire
+Le commentaire s'étend jusqu'en, bout de ligne
+'''
+
+# Déclaration d'une variable
+mavariable = 2
+
+# Affichage
+print(mavariable)
+
+# Le type str représente les chaine de caractères
+print('Bonjour')

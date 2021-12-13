005c22dc0fe84ccfd169009c25738d529a0d2c74
Hakan Kenter <hakan.kt@outlook.fr>
2021-12-13 12:33:49 +0100

    last exercise

--- 3-conditionnelle.py
+++ 3-conditionnelle.py
@@ -1,72 +1,73 @@
-# # # -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 
-# # a = int(input("Entre un nombre : "))
-
-# # '''
-# # Les instructions à l'intérieur  d'un bloc if
-# # ne sont exécutées que si la condition est remplie
-# # '''
-# # if a > 0 : 
-# #     print("Le nombre est positif.")
-# #     print("Oui, j'ai bien dit positif.")
-
-# # # On sort d'un bloc if en revenu en arière dans l'indentation
-# # print("Cette instruction est executée quoi qu'il arrive")
-# # print()
-
-# # # le bloc eslse sert à executer les instructions quand la condition if 
-# # # n'est pas vérifier
-# # if(a > 0): 
-# #     print("positif")
-# # else : 
-# #     print("negatif ou nul")
-# # print()
-
-# # #les blocs elif servent à tester plusieurd conditions les une après les autres 
-# # if a > 0 : 
-# #     print("positif")
-# # elif a < 0 :
-# #     print("negatif")
-# # else : 
-# #     print('nul')
-
-# '''
-# Syntaxe générale des conditionnelles:
-# if condition1 :
-#     instructions1
-#     ...
-# elif condition2 : 
-#     instruction2
-#     ...
-# elif condition3 : 
-#     instructions3
-#     ...
-# else :
-#     instructions par défault
-#     ...
-
-# Les blocs elif sont facultatifs et ils peut y en avoir plusieurs
-# Le bloc else est facultatif mais toujours à la fin et il n'a pas de condition
-# '''
-
-# '''
-# Exercice 
-# Ecrire un programme qui demande à l'utlisateur son age 
-# Si l'age est négatif , afficher "impossible"
-# Si l'âge est entre 0 et 18 ans, afficher 'mineur'
-# Si l'âge est >= 18ans, afficher "majeur"
-# '''
-
-# age = int(input("Quel âge as-tu ?"))
-
-# if age < 0 :
-#     print('Impossible')
-# elif age < 18 :
-#     print("mineur")
-# elif age >= 18 :
-#     print('majeur')
+a = int(input("Entre un nombre : "))
 
 '''
+Les instructions à l'intérieur  d'un bloc if
+ne sont exécutées que si la condition est remplie
+'''
+if a > 0 : 
+    print("Le nombre est positif.")
+    print("Oui, j'ai bien dit positif.")
+
+# On sort d'un bloc if en revenu en arière dans l'indentation
+print("Cette instruction est executée quoi qu'il arrive")
+print()
+
+# le bloc eslse sert à executer les instructions quand la condition if 
+# n'est pas vérifier
+if(a > 0): 
+    print("positif")
+else : 
+    print("negatif ou nul")
+print()
+
+#les blocs elif servent à tester plusieurd conditions les une après les autres 
+if a > 0 : 
+    print("positif")
+elif a < 0 :
+    print("negatif")
+else : 
+    print('nul')
+
+'''
+Syntaxe générale des conditionnelles:
+if condition1 :
+    instructions1
+    ...
+elif condition2 : 
+    instruction2
+    ...
+elif condition3 : 
+    instructions3
+    ...
+else :
+    instructions par défault
+    ...
+
+Les blocs elif sont facultatifs et ils peut y en avoir plusieurs
+Le bloc else est facultatif mais toujours à la fin et il n'a pas de condition
+'''
+
+'''
+Exercice 
+Ecrire un programme qui demande à l'utlisateur son age 
+Si l'age est négatif , afficher "impossible"
+Si l'âge est entre 0 et 18 ans, afficher 'mineur'
+Si l'âge est >= 18ans, afficher "majeur"
+'''
+
+age = int(input("Quel âge as-tu ?"))
+
+if age < 0 :
+    print('Impossible')
+elif age < 18 :
+    print("mineur")
+elif age >= 18 :
+    print('majeur')
+
+'''
+Exercice
 Ecrire un programe qui demande à l'utilisateur son année de naissance
 et son âge.
 Si l'âge ne correspond pas à l'année de naissance, afficher "menteur"
@@ -79,10 +80,29 @@ age2 = int(input("Quel est ton année de naissance ?"))
 
 if (2021-annee2) != age2 :
     print("menteur")
-    if age2 < 0 : 
-        print('impossible')
-else :
-    print('Vrai')
-    if age2 >= 25 :
-        print("Tu es vieux")
+elif age2 < 0 : 
+    print('impossible')
+elif age2 >= 25 :
+    print("Tu es vieux")
     
+'''
+Exercice
+Ecrire un programme qui demande à l'utilisateur son identifiant
+Si l'identifiant est "Thibaut", demander le mot de passe
+Si le mot de passe est "azerty", afficher "Bienvenue", sinon afficher
+"mot de passe incorrect"
+Si l'identifiant n'est pas "Thibaut", afficher "Identifiant inconnu"
+'''
+
+identifiant = "Thibaut"
+mdp = "azerty"
+identifiantQuestion = input("Quel est ton identifiant ?")
+
+if identifiantQuestion == identifiant :
+    mdpQuestion = input("Quel est ton mot de passe ?")  
+    if mdpQuestion == mdp :
+        print('Bienvenue')
+    else : 
+        print('mot de passe incorrect')
+else : 
+    print("Identifiant inconnu")
